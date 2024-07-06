27dce9347180925f6f7e7e343ff3ed619767375f
Juan Hernandez <ing.juanjhdeza@gmail.com>
2024-07-06 02:02:57 -0600

    First Settings

---
+++ src/app/layout.jsx
@@ -0,0 +1,23 @@
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  styles: ["italic", "normal"],
+  subsets: ["latin"]
+});
+
+export const metadata = {
+  title: "INEA Ciudad de México",
+  description: "INEA de la Ciudad de Mexico",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Pagina Inicial</div>
+  )
+}
+
+export default page
