24e7cacf0401b9eb3003c8deb36ba45dee8b5dd7
NORA-NAO <nolasco.ramos.eda@gmail.com>
2024-08-03 16:17:11 -0600

    Ubicacion responsive

--- src/components/Ubicacion.jsx
+++ src/components/Ubicacion.jsx
@@ -5,17 +5,17 @@ const Ubicacion = () => {
   return (
     <div
       id="UbicacionDivP"
-      className=" container mx-auto px-4 py-8"
+      className="container w-auto mx-auto px-4 py-8"
     >
       <h2 className="text-3xl font-semibold tracking-wider text-gray-700 ">
         Ubicación
       </h2>
       <hr className="mb-4 border-gray-200 border-t-2"></hr>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="text-xl text-zinc-800">
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div id="direccion" className="text-s text-zinc-800 ">
           <br />
           <p>Dirección INEA CDMX:</p>
-          <p className="text-xl text-zinc-700">
+          <p className="text-s text-zinc-700">
             Francisco Márquez 160 Planta baja, Colonia Condesa, Cuauhtémoc,
             06140 Ciudad de México, Ciudad de México.
           </p>
@@ -30,9 +30,9 @@ const Ubicacion = () => {
         <div>
           <iframe
             src="https://www.google.com/maps/d/embed?mid=1at8nJJLYa-9bcZEgHMt-8Nb_cPK8tnI&ehbc=2E312F"
-            width="640"
+            width="600"
             height="480"
-            className="w-full h-64 md:h-96 lg:h-[26rem] border-0 rounded-lg"
+            className="w-full h-74 md:h-96 lg:h-[26rem] border-0 rounded-lg"
             allowFullScreen
           ></iframe>
         </div>
