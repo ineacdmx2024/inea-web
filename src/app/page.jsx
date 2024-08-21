97f36e66fe064711def6f6bc631ef3df35567588
Juan Hernandez <ing.juanjhdeza@gmail.com>
2024-08-20 22:33:47 -0600

    Seccion Ligas de Interes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import CarouselBlog from "@/components/CarouselBlog";
 import CarouselOfertEdu from "@/components/CarouselOfertEdu";
-import React from "react";
+import SeccionLigasInte from "@/components/SeccionLigasInte";
 
 function page() {
   return (
@@ -21,8 +21,19 @@ function page() {
           <div className="flex-grow h-px bg-gray-300"></div>
         </div>
       </div>
-
       <CarouselOfertEdu />
+
+      <div className="my-20 mt-36">
+        <h1 className="text-4xl font-medium text-slate-600 mb-2  ">Ligas de Interes</h1>
+        <div className="flex items-center">
+          <div className="w-12 h-2 bg-[#af8900] mt-1"></div>
+          <div className="flex-grow h-px bg-gray-300"></div>
+        </div>
+      </div>
+      <SeccionLigasInte />
+      
+
+
     </main>
   );
 }
