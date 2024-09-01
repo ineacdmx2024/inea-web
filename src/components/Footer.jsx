2b3715b7bf86d56d63a910ae114e9fcf08241f36
KeyBoardLessM <degelhernandezavalos@gmail.com>
2024-08-31 18:06:04 -0600

    espero ya quede bonito =p

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,29 +10,29 @@ const Footer = () => {
 
 
     <footer className="main-footer">
-      <div className="container mx-auto text-left">
-      <div className="flex items-center justify-between py-[1rem] px-4rem ml-[8rem] mr-[8rem]"> 
+      <div className="container mx-auto text-left ">
+      <div className="flex items-center justify-between py-[1rem] px-4rem ml-[8rem] mr-[8rem] my-[5rem] "> 
         <div className="flex flex-wrap center">
           {/* Columna 1: Logos */}
-          <div className="w-full sm:w-1/4 mb-8">
+          <div className="w-full sm:w-1/4 mb-8 ">
             <img
               src="/LogoEducación.svg"
               alt="Logo Educación"
               className="mx-auto max-w-[90%] mt-3"
-              style={{ marginLeft: '0px' }}
+              style={{ marginLeft: '-30px' }}
             />
             <img
               src="/LogoINEACDMX_withe.svg"
               alt="Logo INEACDMX"
               className="mx-auto max-w-[75%] mt-8 p-2 rounded inline-block py-0.3rem mr-1rem leading-relaxed"
-              style={{ marginLeft: '0px' }}
+              style={{ marginLeft: '-30px' }}
             />
           </div>
           
           {/* Columna 2: Mapa del Sitio */}
-          <div className="w-full sm:w-1/4 mb-1">
-            <h1 className="text-lg font-moserrat  ">Mapa del Sitio</h1>
-            <ul className="list-none space-y-2 mt-2">
+          <div className="w-full sm:w-1/4 mb-1 ml-[3rem] mr-[1rem] ">
+            <h1 className="font-moserrat">Mapa del Sitio</h1>
+            <ul className="list-none space-y-2 mt-2 mb-[1rem]">
 
               <li><a href="https://datos.gob.mx/" className="text-white-600 hover:underline hover:text-[#8B6C41]">Trámites y Servicios</a></li>
               <li><a href="https://www.gob.mx/publicaciones" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">¿Quiénes Somos?</a></li>
@@ -48,38 +48,50 @@ const Footer = () => {
           </div>
 
           {/* Columna 3: Contacto */}
-          <div className="w-full sm:w-1/4 mb-8  ">
-            <h5 className="text-lg font-monserrat">Contacto</h5>
+          <div className="w-full sm:w-1/4 mb-8 ml-[3rem] ">
+          <ul> <h1 className=" font-monserrat">Contacto</h1></ul> 
         
-            <ul className="list-none flex jutify-center space-y-10 mt-10  ">
+            <ul className="list-inline flex center space-y-6 mt-5  ">
     
               <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-white-500 hover:underline hover:text-[#8B6C41]">Directorio</a></li>
-              <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-light-blue-500 hover:underline hover:text-[#8B6C41]">correo@inea</a></li>
-        
+              
             </ul>
+            <ul className="list-inline flex center space-y-6 mt-5  ">
+            <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-light-blue-500 hover:underline hover:text-[#8B6C41]">correo@inea</a></li>
+            </ul>
+
        
           </div>
 
           {/* Columna 4: Redes Sociales */}
-          <div className="w-full sm:w-1/4 mb-8">
-            <h1 className="text-lg font-monserrat">Síguenos en:</h1>
+          <div className="   ml-[4rem]">
+            <div className=" font-monserrat "><h1>Síguenos en</h1></div>
+
             <ul className="list-inline flex center space-x-6 mt-5">
+
               <li>
                 <a href="https://www.facebook.com/gobmexico" target="_blank" rel="noopener noreferrer" title="Facebook">
                   <img src="https://framework-gb.cdn.gob.mx/landing/img/facebook.png" alt="Facebook" className="w-8 h-8 " />
                 </a>
               </li>
+
+
+
+
               <li>
                 <a href="https://twitter.com/GobiernoMX" target="_blank" rel="noopener noreferrer" title="Twitter">
                   <img src="https://framework-gb.cdn.gob.mx/landing/img/twitter.png" alt="Twitter" className="w-8 h-8" />
                 </a>
               </li>
+
               <li>
                 <a href="https://www.youtube.com/gobmexico" target="_blank" rel="noopener noreferrer" title="YouTube">
-                  <img src="/images/tutublogo.svg" alt="YouTube" className="w-8 h-8" style={{ marginLeft: '0px', marginTop: '0px' }} />
+                  <img src="/images/tutublogo.svg" alt="YouTube" className="w-9 h-9" style={{ marginLeft: '0px', marginTop: '0px' }} />
                 </a>
               </li>
+
             </ul>
+
           </div>
         </div>
       </div>
