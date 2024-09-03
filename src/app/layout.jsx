2b3d9d4772b22380364548e1fad044e3ac563929
KeyBoardLessM <109236354+KeyBoardLessM@users.noreply.github.com>
2024-09-03 12:43:19 -0600

    footer
    ya acabé, ya no acepto cambios ni devoluciones, YA ACABÉ

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <NavBar />
+       {/* <NavBar >*/}
         {children}
         <Footer />
       </body>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,36 +9,36 @@ const Footer = () => {
   return (
 
 
-    <footer className="main-footer">
+    <footer className="main-footer relative">
       <div className="container mx-auto text-left ">
 
 
 
 
-      <div className="flex items-center justify-between py-[1rem] px-4rem ml-[8rem] mr-[8rem] my-[5rem] "> 
+      <div className="flex items-center justify-between py-[1rem] px-4rem ml-[2.5rem]  my-[5rem] "> 
         <div className="flex flex-wrap center">
           {/* Columna 1: Logos */}
-          <div className="w-full sm:w-1/4 mb-5 ml-[3rem] ">
+          <div className="w-full sm:w-1/4 mb-5  ">
             <img
               src="/LogoEducación.svg"
               alt="Logo Educación"
               className="mx-auto max-w-[90%] mt-3"
-              style={{ marginLeft: '-30px' }}
+              style={{ marginLeft: '-0px' }}
             />
             <img
               src="/LogoINEACDMX_withe.svg"
               alt="Logo INEACDMX"
               className="mx-auto max-w-[75%] mt-8 p-2 rounded inline-block py-0.3rem mr-1rem leading-relaxed"
-              style={{ marginLeft: '-30px' }}
+              style={{ marginLeft: '-0px' }}
             />
           </div>
           
           {/* Columna 2: Mapa del Sitio */}
-          <div className="w-full sm:w-1/4 mb-5 ml-[3rem] ">
-            <h1 className="text-2xl font Monserrat " style={{fontSize:"25px"}}>Mapa del Sitio</h1> 
-            <ul className="list-none space-y-4 mt-2 mb-[1rem]">
+          <div className="w-full sm:w-1/4 mb-5 ml-[3.5rem] ">
+          <ul> <h1 className="text-2xl font Monserrat " style={{fontSize:"18px"}}>Mapa del Sitio</h1> </ul>
+            <ul className="list-none space-y-3 mt-5 mb-[1rem] text-[14px]">
 
-              <li><a href="https://datos.gob.mx/" className="text-white-600 hover:underline hover:text-[#8B6C41]">Trámites y Servicios</a></li>
+              <li><a href="https://datos.gob.mx/" className="text-white-600 hover:underline hover:text-[#8B6C41] ]">Trámites y Servicios</a></li>
               <li><a href="https://www.gob.mx/publicaciones" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">¿Quiénes Somos?</a></li>
               <li><a href="http://www.inea.gob.mx/Transparencia1/transparencia.html" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">Aviso de privacidad</a></li>
               <li><a href="https://www.infomex.org.mx/gobiernofederal/home.action" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">Comité de Ética</a></li>
@@ -52,26 +52,29 @@ const Footer = () => {
           </div>
 
           {/* Columna 3: Contacto */}
-          <div className="w-full sm:w-1/4 mb-5 ml-[3rem]">
-          <ul> <h1  className="text-2xl font Monserrat" style={{fontSize:"25px"}}>Contacto</h1></ul> 
+          <div className="w-full sm:w-1/4 mb-5 ml-[1rem]">
+          <ul> <h1  className="text-2xl font Monserrat" style={{fontSize:"18px"}}>Contacto</h1></ul> 
         
-            <ul className="list-inline flex center space-y-6 mt-5  ">
+            <ul className="list-none flex center space-y-6 mt-5 text-[16px] ">
     
-              <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-white-500 hover:underline hover:text-[#8B6C41]">Directorio</a></li>
+              <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-white-500 hover:underline hover:text-[#8B6C41 ] ">Directorio</a></li>
               
             </ul>
-            <ul className="list-inline flex center space-y-0 mt-300  ">
-            <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-light-blue-500 hover:underline hover:text-[#8B6C41]">correo@inea</a></li>
+            <ul className="list-inline flex center space-y-0 mt-0  ">
+            <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-light-blue-500 hover:underline hover:text-[#8B6C41] ">correo@inea</a></li>
             </ul>
 
        
           </div>
 
-          {/* Columna 4: Redes Sociales */}
-          <div className=" w-full sm:w-1/4 py-200 px-100">
-            <h1  className="text-2xl font Monserrat" style={{fontSize:"25px"}}>Síguenos en</h1>
 
-            <ul className="list-inline flex center space-x-6 mt-5">
+
+          {/* Columna 4: Redes Sociales */}
+        <div className="w-full sm:w-1/4 mb-5 ml-[-8rem] ">
+          <div className="flex flex-col items-end"> 
+            <h1  className="text-2xl font Monserrat" style={{fontSize:"18px"}}>Síguenos en</h1>
+
+            <ul className="list-inline flex space-x-5 mt-5">
 
               <li>
                 <a href="https://www.facebook.com/gobmexico" target="_blank" rel="noopener noreferrer" title="Facebook">
@@ -97,6 +100,7 @@ const Footer = () => {
             </ul>
 
           </div>
+          </div>
         </div>
 
 
@@ -109,10 +113,7 @@ const Footer = () => {
         </div>
 
 
-
- <div className=" p-6 footer-pleca flex justify-start w-full lg:w-[calc(100vw - 14rem)] lg:absolute lg:left-0">
- 
-      </div>
+      
 
 
 
@@ -122,7 +123,8 @@ const Footer = () => {
       </div>
 
 
-    
+      <img src="/images/pleca_footer.svg" alt="plecabnj" className="w-full h-auto"></img> 
+
 
 
     </footer>
@@ -139,7 +141,9 @@ export default Footer;
 
 
 //
-//
+// <div className=" footer-pleca p-6  flex justify-start w-full lg:w-[calc(100vw - 14rem)] lg:absolute lg:left-0 h-auto">
+ 
+//</div>
 // <img src="/images/pleca_footer.svg" className="container-fluid  mb-[-6rem]   w-[100%] flex  leading-relaxed" style={{  borderRadius:'10%'}}></img>
 
 //https://docs.strapi.io/dev-docs/quick-start#:~:text=Step%201%3A%20Log%20in%20to%20the%20admin%20panel,Publish%20the%20content%20Step%206%3A%20Use%20the%20API
