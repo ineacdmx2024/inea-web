d4d478e54ae1dab69bdc6422964b03a439c8436d
M43G0R <ing.juanjhdeza@gmail.com>
2024-09-03 11:35:40 -0600

    Banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,11 @@ const open_Sans = Open_Sans({
 function page() {
   return (
     <main className={`mx-auto ${open_Sans.className} bg-white`}>
-      
+
+      <div>
+        <img src="/Banner_INEA_CDMX_Educacion.jpg" alt="Banner" className="w-full h-auto" />
+      </div>
+
       <div className="mx-auto mt-20 mb-16 w-11/12 medida3:w-4/5 arrow:w-[750px] tablet:w-[1170px]">
         <h1 className="text-2xl font-medium text-slate-700 mb-2 letras:text-3xl ">Blog</h1>
         <div className="flex items-center">
@@ -49,7 +53,7 @@ function page() {
         </div>
       </div>
       <Ubicacion />
-      
+
     </main>
   );
 }
