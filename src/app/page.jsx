a782886faf5b6d847c0930ffd22183ed1bd4a83c
NORA-NAO <nolasco.ramos.eda@gmail.com>
2024-09-08 13:46:13 -0600

    Footer corregido y responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -54,7 +54,7 @@ function page() {
         </div>
       </div>
       <Ubicacion />
-
+      <br />
     </main>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,100 +8,93 @@ const Footer = () => {
 
   return (
 
-    <footer className="main-footer relative">
-      <div className="container mx-auto text-left ">
-
-      <div className="flex items-center justify-between py-[1rem] px-4rem ml-[2.5rem]  mt-[5rem]">
-        <div className="flex flex-wrap justify-center mx-auto">
-          {/* Columna 1: Logos */}
-          <div className="w-full sm:w-1/4 mb-5  ">
-            <img
-              src="/LogoEducacion.svg"
-              alt="Logo Educación"
-              className="mx-auto max-w-[90%] mt-3"
-              style={{ marginLeft: '-0px' }}
-            />
-            <img
-              src="/LogoINEACDMX_withe.svg"
-              alt="Logo INEACDMX"
-              className="mx-auto max-w-[75%] mt-8 p-2 rounded inline-block py-0.3rem mr-1rem leading-relaxed"
-              style={{ marginLeft: '-0px' }}
-            />
+    <footer className=" bg-[#13322B] h-auto">
+      <div className="p-8 mx-auto max-w-screen-xl sm:p-6" id='content'>
+        <div className="md:flex" id='Content'>
+          <div className="mb-6 md:mb-0">
+            <a href="https://flowbite.com" className="flex items-center">
+              <img src="/LogoEducacion.svg" className="mr-3 h-12" alt="Logo Educacion" />
+            </a>
+            <br />
+            <a href="/" className="flex items-center">
+              <img src="/LogoINEACDMX_withe.svg" className="mr-3 h-[70px]" alt="Logo INEA" />
+            </a>
           </div>
-
-          {/* Columna 2: Mapa del Sitio */}
-          <div className="w-full sm:w-1/4 mb-5 ml-[3.5rem] ">
-          <ul> <h1 className="text-2xl font Monserrat " style={{fontSize:"18px"}}>Mapa del Sitio</h1> </ul>
-            <ul className="list-none space-y-3 mt-5 mb-[1rem] text-[14px]">
-
-              <li><a href="https://datos.gob.mx/" className="text-white-600 hover:underline hover:text-[#8B6C41] ]">Trámites y Servicios</a></li>
-              <li><a href="https://www.gob.mx/publicaciones" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">¿Quiénes Somos?</a></li>
-              <li><a href="http://www.inea.gob.mx/Transparencia1/transparencia.html" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">Aviso de privacidad</a></li>
-              <li><a href="https://www.infomex.org.mx/gobiernofederal/home.action" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">Comité de Ética</a></li>
-              <li><a href="http://www.inai.org.mx/" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">Transparencia</a></li>
-              <li><a href="http://alertadores.funcionpublica.gob.mx/" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">Glosario</a></li>
-              <li><a href="https://www.gob.mx/inea" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">INEA Nacional</a></li>
-              <li><a href="https://www.gob.mx" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">Gobierno</a></li>
-              <li><a href="http://sidec.funcionpublica.gob.mx/" target="_blank" rel="noopener noreferrer" className="text-white-600 hover:underline hover:text-[#8B6C41]">Créditos</a></li>
-
-            </ul>
-          </div>
-
-          {/* Columna 3: Contacto */}
-          <div className="w-full sm:w-1/4 mb-5 ml-[1rem]">
-          <ul> <h1  className="text-2xl font Monserrat" style={{fontSize:"18px"}}>Contacto</h1></ul>
-
-            <ul className="list-none flex center space-y-6 mt-5 text-[16px] ">
-
-              <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-white-500 hover:underline hover:text-[#8B6C41 ] ">Directorio</a></li>
-
-            </ul>
-            <ul className="list-inline flex center space-y-0 mt-0  text-[16px] ">
-            <li><a href="mailto:atencion_inea@inea.gob.mx" className="text-light-blue-500 hover:underline hover:text-[#8B6C41] ">correo@inea</a></li>
-            </ul>
-
-
-          </div>
-
-
-
-          {/* Columna 4: Redes Sociales */}
-        <div className="w-full sm:w-1/4 mb-5 ml-[-8rem] ">
-          <div className="flex flex-col items-end">
-            <h1  className="text-2xl font Monserrat" style={{fontSize:"18px"}}>Síguenos en</h1>
-
-            <ul className="list-inline flex space-x-5 mt-5">
-
-              <li>
-                <a href="https://www.facebook.com/gobmexico" target="_blank" rel="noopener noreferrer" title="Facebook">
-                  <img src="https://framework-gb.cdn.gob.mx/landing/img/facebook.png" alt="Facebook" className="w-8 h-8 " />
+          <div className="grid grid-cols-1 gap-8 sm:gap-24 sm:grid-cols-3">
+            <div>
+              <h2 className="mb-3 text-sm text-gray-900 uppercase dark:text-white">Mapa de Sitio</h2>
+              <ul className="text-gray-600 dark:text-white">
+                <li>
+                  <a href="https://datos.gob.mx/" className="hover:underline">Trámites y Servicios</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">¿Quiénes Somos?</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">Aviso de privacidad</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">Comité de Ética</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">Transparencia</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">Glosario</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">INEA Nacional</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">Gobierno</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">Créditos</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm text-gray-900 uppercase dark:text-white">Contacto</h2>
+              <ul className="text-gray-600 dark:text-white">
+                <li>
+                  <a href="/" className="hover:underline ">Directorio</a>
+                </li>
+                <li>
+                  <a href="/" className="hover:underline">Correo@inea.gob.mx</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm text-gray-900 uppercase dark:text-white">Síguenos en</h2>
+              <div className="flex mt-4 space-x-3 sm:justify-left sm:mt-0">
+                <a href="/" className="text-white hover:text-gray-900 dark:hover:text-white">
+                  <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
                 </a>
-              </li>
-
-
-              <li>
-                <a href="https://twitter.com/GobiernoMX" target="_blank" rel="noopener noreferrer" title="Twitter">
-                  <img src="https://framework-gb.cdn.gob.mx/landing/img/twitter.png" alt="Twitter" className="w-8 h-8" />
+                <a href="/" className="text-white hover:text-gray-900 dark:hover:text-white">
+                  <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                 </a>
-              </li>
-
-              <li>
-                <a href="https://www.youtube.com/gobmexico" target="_blank" rel="noopener noreferrer" title="YouTube">
-                  <img src="/tutublogo.svg" alt="YouTube" className="w-9 h-9" style={{ marginLeft: '0px', marginTop: '0px' }} />
+                <a href="/" className="text-white hover:text-gray-900 dark:hover:text-white">
+                <svg className="h-8 w-8"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />  <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" /></svg>
                 </a>
-              </li>
-
-            </ul>
-
-          </div>
+              </div>
+            </div>
           </div>
         </div>
 
-        </div>
+        <br />
+        {/*
+        <div className="sm:flex sm:items-center sm:justify-center">
+          <span className="text-center text-gray-500 sm:text-center dark:text-gray-400">© 2024. All Rights Reserved.
+          </span>
+          <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+          </div>
+        </div> */}
 
+      </div >
+      <div className='pleca'>
+        <img src="/Pleca_footer.svg" alt="" />
       </div>
 
-      <img src="/Pleca_footer.svg" alt="plecabnj" className="w-full h-auto"/>
 
     </footer>
 
