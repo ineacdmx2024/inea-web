327684db3df2e9936be1b0e26beb5796da2ca46b
Juan Hernandez <ing.juanjhdeza@gmail.com>
2024-09-18 04:52:55 -0600

    Correcciones oferta educativa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,9 +10,8 @@ module.exports = {
         "tablet": "1210px",
         "arrow": "810px",
         "medida3" : "750px",
-        "letras" : "400px",
-        "ofertaEdu" : "500px",
-        "mobileEdu" : "325px"
+        "letras" : "390px",
+        "ofertaEdu" : "500px"
 
       },
       maxWidth: {
