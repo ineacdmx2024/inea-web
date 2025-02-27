58401728965e3b3845e82024b8fb70a3105b4994
Gapuccino <gabrielpin2121@gmail.com>
2025-02-27 15:42:12 -0600

    Merged try

--- src/components/EnlacesR_Lateral.jsx
+++ src/components/EnlacesR_Lateral.jsx
@@ -35,19 +35,36 @@ const Card = ({ title, imageSrc, buttonText, link }) => {
 =======
     <Link href={link}>
 <<<<<<< HEAD
+<<<<<<< HEAD
       <div className="mb-8 border border-slate-300 rounded-lg h-[420px] p-5 pt-8 flex flex-col items-center mx-4">
         <div className="flex flex-col items-center h-[340px]">
           <div className="w-[300px] h-[200px] overflow-hidden rounded-lg">
+=======
+      <div className="border border-slate-300 shadow-none rounded-lg h-fit p-8 flex flex-col mb-8">
+        <div className="flex flex-col items-center">
+          {/* Imagen */}
+          <div className="w-full aspect-[4/3] relative mb-4">
+>>>>>>> 1ec61ba (Carrusel)
             <img
               src={imageSrc}
               alt={title}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover rounded-lg"
             />
           </div>
-          <h3 className="h-16 overflow-hidden mt-4 px-4 text-center text-[18px] font-medium text-[#333334]">
-            {truncateText(title, 35)}
+
+          {/* Titulo */}
+          <h3 className="text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium mb-2">
+            {truncateText(title, 21)}
           </h3>
+
+          {/* Botón */}
+          <div className="flex justify-center mt-auto">
+            <button className="bg-[#611232] text-white text-xs py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] mx-auto block">
+              {buttonText}
+            </button>
+          </div>
         </div>
+<<<<<<< HEAD
         <button
           className="bg-[#611232] text-white 
           hover:border-[#611232] hover:border-2 hover:bg-white hover:text-[#611232] 
@@ -81,6 +98,8 @@ const Card = ({ title, imageSrc, buttonText, link }) => {
           </div>
         </div>
 >>>>>>> Gabriel-prueba
+=======
+>>>>>>> 1ec61ba (Carrusel)
       </div>
     </Link>
   );
