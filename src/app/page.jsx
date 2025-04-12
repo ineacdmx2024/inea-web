e3eecbb88674d9b2e824882c3572a5a56d8a9859
Jaret3000 <jaretitosayuso@gmail.com>
2025-04-12 17:33:22 -0600

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -66,30 +66,29 @@ function Page() {
       </div>
       <CarouselOfertEdu />
 
-     <div className="mx-auto w-11/12 medida3:w-4/5 arrow:w-[750px] tablet:w-[1170px]">
-       <h1 className="text-2xl font-medium text-slate-700 mb-2 letras:text-3xl">
-         Enlaces de interés
-       </h1>
-       <div className="flex items-center">
-         <div className="w-9 h-[5px] bg-[#b38e61] mt-1"></div>
-         <div className="flex-grow h-px bg-gray-300"></div>
-       </div>
-     </div>
-
-<SeccionLigasInte />
-      
-      <div className="mx-auto w-11/12 medida3:w-4/5 arrow:w-[750px] tablet:w-[1170px] mt-[97px] mb-2">
-        <div className="mb-6">
-          <h1 className="text-2xl font-medium text-slate-700 letras:text-3xl">
-            Ubicación
-          </h1>
-          <div className="flex items-center mt-1">
-            <div className="w-9 h-[5px] bg-[#b38e61]"></div>
-            <div className="flex-grow h-px bg-gray-300"></div>
-          </div>
+     <div className="my-20 mx-auto w-11/12 medida3:w-4/5 arrow:w-[750px] tablet:w-[1170px]">
+        <h1 className="text-2xl font-medium text-slate-700 mb-2 letras:text-3xl">
+          Enlaces de interés
+        </h1>
+        <div className="flex items-center">
+          <div className="w-9 h-[5px] bg-[#b38e61] mt-1"></div>
+          <div className="flex-grow h-px bg-gray-300"></div>
         </div>
-        <Ubicacion />
       </div>
+      <SeccionLigasInte />
+
+        <div className="mx-auto w-11/12 medida3:w-4/5 arrow:w-[750px] tablet:w-[1170px] mt-4 mb-2">
+          <div className="mb-6">
+            <h1 className="text-2xl font-medium text-slate-700 letras:text-3xl">
+              Ubicación
+            </h1>
+            <div className="flex items-center mt-1">
+              <div className="w-9 h-[5px] bg-[#b38e61]"></div>
+              <div className="flex-grow h-px bg-gray-300"></div>
+            </div>
+          </div>
+          <Ubicacion />
+        </div>
     </main>
   );
 }
