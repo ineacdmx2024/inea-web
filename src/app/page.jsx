74ad449b28640fec284efa5f6c7f754f8138ae25
Jaret3000 <jaretitosayuso@gmail.com>
2025-04-14 14:27:10 -0600

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -79,16 +79,18 @@ function Page() {
 
       {/* Ubicación */}
       <section className="mt-[97px] mb-2 px-4 flex flex-col items-center">
-        <div className="mb-6 max-w-[1170px] w-full">
-          <h1 className="text-2xl font-medium titulo-seccion text-[#333334] letras:text-3xl">
-            Ubicación
-          </h1>
-          <div className="flex items-center mt-1">
-            <div className="w-9 h-[5px] bg-[#b38e61]"></div>
-            <div className="flex-grow h-px bg-gray-300"></div>
+        <div className="w-full max-w-[1142px]">
+          <div className="mb-6">
+            <h1 className="text-2xl font-medium titulo-seccion text-[#333334] letras:text-3xl">
+              Ubicación
+            </h1>
+            <div className="flex items-center mt-1">
+              <div className="w-9 h-[5px] bg-[#b38e61]"></div>
+              <div className="flex-grow h-px bg-gray-300"></div>
+            </div>
           </div>
+          <Ubicacion />
         </div>
-        <Ubicacion />
       </section>
     </main>
   );
