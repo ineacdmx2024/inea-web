3745571708a4c8dcab2adbce70530308943ae257
Jaret3000 <jaretitosayuso@gmail.com>
2025-04-14 20:11:25 -0600

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { Montserrat } from "next/font/google";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 
@@ -9,6 +10,12 @@ const roboto = Roboto({
   subsets: ["latin"],
 });
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat", // usa variable para aplicar vía clase
+  display: "swap",
+});
+
 export const metadata = {
   title: "INEA Ciudad de México",
   description: "INEA de la Ciudad de Mexico",
@@ -16,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.variable}>
       {/* <link
         href="https://framework-gb.cdn.gob.mx/assets/styles/main.css"
         rel="stylesheet"
@@ -28,7 +35,7 @@ export default function RootLayout({ children }) {
         key="gob-mx-js"
         defer
       ></script> */}
-      <body className={roboto.className}>
+      <body className={`${roboto.className} font-montserrat`}>
         <NavBar />
         {children}
         <Footer/>
