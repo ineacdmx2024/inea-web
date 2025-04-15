ac2a867331a6fb4c4e941997dfcf04dd24cd2571
Jaret3000 <jaretitosayuso@gmail.com>
2025-04-14 18:36:11 -0600

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Open_Sans, Montserrat } from "next/font/google";
-import "../../app/globals.css";
 import CarouselBlog from "@/components/CarouselBlog";
 import CarouselInicio from "@/components/CarouselInicio";
 import CarouselOfertEdu from "@/components/CarouselOfertEdu";
