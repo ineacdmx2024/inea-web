7c773e612f15b03f2653f5c60698ac865a494745
Jaret3000 <jaretitosayuso@gmail.com>
2025-04-16 19:25:21 -0600

    Update CarouselOfertEdu.jsx

--- src/components/CarouselOfertEdu.jsx
+++ src/components/CarouselOfertEdu.jsx
@@ -104,7 +104,7 @@ function CarouselOfertEdu() {
     autoplaySpeed: 5000,
     dotsClass: "slick-dots custom-dots",
     appendDots: dots => (
-      <div className="mt-0 flex justify-center w-full">
+      <div className="mt-2 flex justify-center w-full">
         <ul className="flex space-x-2">{dots}</ul>
       </div>
     ),
@@ -119,6 +119,8 @@ function CarouselOfertEdu() {
           margin-top: 0 !important;
           padding: 0;
           position: relative !important;
+          display: flex;
+          justify-content: center;
         }
 
         .custom-dots li {
@@ -147,19 +149,17 @@ function CarouselOfertEdu() {
             {modalidades.map((noticia, index) => (
               <div key={index * 36} className="tablet:h-[450px] pt-4 tablet:pt-0">
                 <Link href={`/oferta-educativa${noticia.url}`} className="block h-full">
-                  <div className="border-0 tablet:border border-slate-300 tablet:shadow-none rounded-none tablet:rounded-lg h-full p-2 tablet:p-8 flex flex-col carousel-card">
-                    <div className="image-container mb-4">
+                  <div className="border-0 tablet:border border-slate-300 tablet:shadow-none rounded-none tablet:rounded-lg h-full p-2 tablet:p-6 flex flex-col justify-between">
+                    <div className="image-container mb-2">
                       <img src={noticia.image || "/placeholder.svg"} alt={noticia.name} />
                     </div>
-                    <div className="flex flex-col justify-between flex-grow">
-                      <h3 className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium h-[32px]">
-                        {truncateText(noticia.name, 37)}
-                      </h3>
-                      <div className="flex justify-center mt-10 tablet:mt-4">
-                        <button className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light">
-                          Ir al sitio
-                        </button>
-                      </div>
+                    <h3 className="mt-2 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium">
+                      {truncateText(noticia.name, 37)}
+                    </h3>
+                    <div className="flex justify-center mt-4">
+                      <button className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light">
+                        Ir al sitio
+                      </button>
                     </div>
                   </div>
                 </Link>
