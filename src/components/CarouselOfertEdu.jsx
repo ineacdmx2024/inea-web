1d02cb036b061cc4069089c65d055ea37effbda5
Jaret3000 <jaretitosayuso@gmail.com>
2025-04-16 18:32:54 -0600

    Update CarouselOfertEdu.jsx

--- src/components/CarouselOfertEdu.jsx
+++ src/components/CarouselOfertEdu.jsx
@@ -104,7 +104,7 @@ function CarouselOfertEdu() {
     autoplaySpeed: 5000,
     dotsClass: "slick-dots custom-dots",
     appendDots: dots => (
-      <div className="-mt-2 flex justify-center w-full">
+      <div className="-mt-5 flex justify-center w-full">
         <ul className="flex space-x-2">{dots}</ul>
       </div>
     ),
