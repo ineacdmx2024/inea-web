644ce4079fb38b334dad548b6fb44cdd570c434e
chlrobert93 <chlrobert93@gmail.com>
2025-04-24 18:45:52 -0600

    Cambios imagen

--- src/components/CarouselInicio.jsx
+++ src/components/CarouselInicio.jsx
@@ -107,7 +107,7 @@ function CarouselInicio() {
       <div>
         <Slider {...settings}>
           {restantes.map((restante, index) => (
-            <div key={index} className="w-full m-auto hover:cursor-pointer">
+            <div key={index} className="hover:cursor-pointer">
               {/* Imagen para dispositivos de escritorio */}
               {/* <div className="w-full h-full hidden md:block"> */}
               <div className=" hidden md:block relative">
