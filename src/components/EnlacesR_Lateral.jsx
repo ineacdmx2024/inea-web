a11e403770c05430fabc5dc85a42bcb37be6a08e
Esteban Marquez <marquezriosesteban@gmail.com>
2025-04-28 14:26:06 -0600

    refactor(Card): enhance layout and styling for improved responsiveness
    Updated the Card component to enhance its layout and styling, ensuring better responsiveness. Adjusted padding, border styles, and text sizes for a more cohesive design across devices. The button placement has also been modified for improved user interaction.

--- src/components/EnlacesR_Lateral.jsx
+++ src/components/EnlacesR_Lateral.jsx
@@ -10,22 +10,25 @@ const truncateText = (text, maxLetters) => {
 
 const Card = ({ title, imageSrc, buttonText, link }) => {
   return (
-    <Link href={link} className="block">
-      <div className="mb-8 border border-slate-300 rounded-lg h-[400px] p-5 flex flex-col items-center justify-between">
-        {/* Contenedor principal de la tarjeta */}
-        <div className="flex flex-col items-center">
+    <Link href={link} className="block h-full">
+      <div className="border-0 tablet:border border-slate-300 tablet:shadow-none rounded-none tablet:rounded-lg h-full p-2 tablet:p-8 flex flex-col carousel-card">
+        <div className="image-container mb-4">
           <img
             src={imageSrc}
             alt={title}
             className="w-full h-[200px] object-cover rounded-lg"
           />
-          <h3 className="text-center text-[18px] font-medium mt-4">
-            {truncateText(title, 40)}
-          </h3>
         </div>
-        <button className="bg-[#611232] text-white text-xs py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] mx-auto block mt-4">
-          {buttonText}
-        </button>
+        <div className="flex flex-col justify-between flex-grow">
+          <h3 className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium h-[32px]">
+            {truncateText(title, 37)}
+          </h3>
+          <div className="flex justify-center mt-10 tablet:mt-4">
+            <button className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light">
+              {buttonText}
+            </button>
+          </div>
+        </div>
       </div>
     </Link>
   );
