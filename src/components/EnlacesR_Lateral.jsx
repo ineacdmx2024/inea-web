4eecc6ecf89c1c3d8c10c59df23d06a5160ee5b1
Esteban Marquez <marquezriosesteban@gmail.com>
2025-04-28 17:21:48 -0600

    refactor(Card): update styling for improved layout and user experience
    Enhanced the Card component by adding a background color, adjusting the layout, and refining button styles for better responsiveness. Updated text sizes and margins for improved visual consistency across devices.

--- src/components/EnlacesR_Lateral.jsx
+++ src/components/EnlacesR_Lateral.jsx
@@ -20,6 +20,7 @@ const Card = ({ title, imageSrc, buttonText, link }) => {
           overflow: hidden;
           border-radius: 8px;
           margin: 0 auto;
+          background-color: #f5f5f5;
         }
         
         .image-container img {
@@ -42,9 +43,9 @@ const Card = ({ title, imageSrc, buttonText, link }) => {
           }
         }
       `}</style>
-      <Link href={link} className="block h-full w-full flex justify-center">
-        <div className="border-0 tablet:border border-slate-300 tablet:shadow-none rounded-none tablet:rounded-lg h-full p-2 tablet:p-8 flex flex-col carousel-card w-full max-w-[300px] letras:max-w-[360px] ofertaEdu:max-w-[400px] tablet:max-w-[1150px]">
-          <div className="image-container mb-4">
+      <Link href={link} className="block h-full w-full">
+        <div className="bg-white border border-slate-200 rounded-lg h-full p-6 flex flex-col w-[362px] h-[450px]max-w-[800px] letras:max-w-[900px] ofertaEdu:max-w-[1000px] tablet:max-w-[1800px]">
+          <div className="image-container mb-6">
             <img
               src={imageSrc}
               alt={title}
@@ -52,11 +53,11 @@ const Card = ({ title, imageSrc, buttonText, link }) => {
             />
           </div>
           <div className="flex flex-col justify-between flex-grow">
-            <h3 className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium h-[32px] mb-8">
+            <h3 className="text-center text-[18px] tablet:text-[22px] text-[#333334] font-medium mb-6">
               {truncateText(title, 37)}
             </h3>
-            <div className="flex justify-center mt-10 tablet:mt-4">
-              <button className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light">
+            <div className="flex justify-center mt-4">
+              <button className="bg-[#611232] text-white text-sm py-2.5 px-6 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-medium transition-all duration-300">
                 {buttonText}
               </button>
             </div>
