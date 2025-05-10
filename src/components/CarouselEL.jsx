d4f083fcfdef63c6cc7498f679b9739612b4530e
Jaret3000 <jaretitosayuso@gmail.com>
2025-05-09 22:17:56 -0600

    Update CarouselEL.jsx

--- src/components/CarouselEL.jsx
+++ src/components/CarouselEL.jsx
@@ -103,7 +103,7 @@ const CarouselEL = ({ cards }) => {
         }
       `}</style>
       <div className="md:hidden w-full">
-        <div className="border border-slate-300 rounded-lg bg-white mx-auto w-[340px] pb-8 overflow-visible ml-4">
+        <div className="border border-slate-300 rounded-lg bg-white mx-auto w-[340px] pb-8 overflow-visible -ml-2">
           <Slider {...settings}>
             {cards.map((card, index) => (
               <div key={index} className="pt-6 px-6 outline-none">
