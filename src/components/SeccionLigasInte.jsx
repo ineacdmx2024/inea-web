5128ab1f0da3804cba3f74c240110629cd442f41
Jaret3000 <jaretitosayuso@gmail.com>
2025-05-21 20:19:04 -0600

    Update SeccionLigasInte.jsx

--- src/components/SeccionLigasInte.jsx
+++ src/components/SeccionLigasInte.jsx
@@ -18,7 +18,7 @@ function PrevArrow(props) {
   const { className, style, onClick } = props
   return (
     <div
-      className={`${className} !z-10 before:!content-none`}
+      className={${className} !z-10 before:!content-none}
       style={{
         ...style,
         display: "block",
@@ -44,7 +44,7 @@ const NextArrow = (props) => {
   const { className, style, onClick } = props
   return (
     <div
-      className={`${className} !z-10 before:!content-none ml-5`}
+      className={${className} !z-10 before:!content-none ml-5}
       style={{
         ...style,
         display: "block",
@@ -139,7 +139,7 @@ function SeccionLigasInte() {
       window.open(item.link, "_blank") // Abre en una nueva pestaña
     } else if (item && item.slug) {
       ;<DetalleEnlace slug={item.slug} />
-      router.push(`/home-enlaces-de-interes/${item.slug}`)
+      router.push(/home-enlaces-de-interes/${item.slug})
 
       console.log(item.slug.toString())
     } else {
@@ -167,7 +167,7 @@ function SeccionLigasInte() {
 
   return (
     <>
-      <style jsx global>{`
+      <style jsx global>{
         .custom-dots {
           bottom: -20px;
           display: flex;
@@ -245,19 +245,18 @@ function SeccionLigasInte() {
         /* Dimensiones fijas para imágenes */
         .image-container {
           width: 100%;
-          max-width: 296px;
-          height: 236.8px;
+          max-width: 296px; /* Ancho fijo de 296px */
+          height: 236.8px; /* Alto fijo de 236.8px */
           position: relative;
           overflow: hidden;
-          border-radius: 1rem;
+          border-radius: 0.5rem;
           margin: 0 auto;
         }
-
+        
         .image-container img {
           width: 100%;
           height: 100%;
           object-fit: cover;
-          border-radius: 1rem;
         }
         
         /* Medidas responsivas para dispositivos móviles */
@@ -265,35 +264,46 @@ function SeccionLigasInte() {
           .image-container {
             width: 100%;
             height: 0;
-            padding-bottom: 80%;
+            padding-bottom: 80%; /* Mantiene proporción similar en móviles */
             max-width: none;
           }
-
+          
           .image-container img {
             position: absolute;
             top: 0;
             left: 0;
           }
         }
-      `}</style>
+      }</style>
 
 
-     <div className="p-2 pt-0 mt-12">
-       {/* Desktop fixed links */}
+      <div className="p-2 pt-0 mt-12">
+        {/* Desktop fixed links */}
         <div className="fijas justify-center items-center !z-5 w-4/5 tablet:w-[1150px] mx-auto hidden tablet:flex gap-8">
-          {fijos.map((fijo, index) => (
+          {fijos.map((fijos, index) => (
             <div className="w-1/3 cursor-pointer" key={index}>
-              <Link href={`/home-enlaces-de-interes/${fijo.slug}`} target="_self" className="block h-full">
-                <div className="border border-slate-300 shadow-none rounded-[1rem] h-[450px] p-2 tablet:p-8 flex flex-col carousel-card">
+              <Link
+                href={/home-enlaces-de-interes/${fijos.slug}}
+                target="_self"
+                className="block h-full"
+              >
+                <div className="border border-slate-300 shadow-none rounded-lg h-[450px] p-2 tablet:p-8 flex flex-col carousel-card">
                   <div className="image-container mb-4">
-                    <img src={fijo.imagen || "/placeholder.svg"} alt={fijo.titulo} />
+                    <img
+                      src={fijos.imagen || "/placeholder.svg"}
+                      alt={fijos.titulo}
+                    />
                   </div>
                   <div className="flex flex-col justify-between flex-grow">
-                    <h3 className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium w-[17ch] break-normal h-[32px]">
-                      {truncateText(fijo.titulo, 37)}
+                    <h3
+                      className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium  w-[17ch] break-normal h-[32px]"
+                    >
+                      {truncateText(fijos.titulo, 37)}
                     </h3>
                     <div className="flex justify-center mt-10 tablet:mt-4">
-                      <button className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light">
+                      <button
+                        className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light"
+                      >
                         Ir al sitio
                       </button>
                     </div>
@@ -306,20 +316,34 @@ function SeccionLigasInte() {
 
         {/* Mobile fixed links carousel*/}
         <div className="carrusel tablet:hidden px-4">
-          <Slider {...settings} className="bg-white border border-slate-300 rounded-[1rem] mx-auto w-full max-w-sm pb-10">
-            {fijos.map((fijo, index) => (
+          <Slider
+          {...settings}
+          className="bg-white border border-slate-300 rounded-lg mx-auto w-full max-w-sm pb-10"
+          >
+            {fijos.map((fijos, index) => (
               <div key={index} className="pt-4 tablet:pt-0">
-                <Link href={`/home-enlaces-de-interes/${fijo.slug}`} target="_self" className="block h-full">
-                  <div className="border border-slate-300 shadow-none rounded-[1rem] h-full p-2 tablet:p-8 flex flex-col carousel-card">
+                <Link
+                  href={/home-enlaces-de-interes/${fijos.slug}}
+                  target="_self"
+                  className="block h-full"
+                >
+                  <div className="border-0 tablet:border border-slate-300 tablet:shadow-none rounded-none tablet:rounded-lg h-full p-2 tablet:p-8 flex flex-col carousel-card">
                     <div className="image-container mb-4">
-                      <img src={fijo.imagen || "/placeholder.svg"} alt={fijo.titulo} />
+                      <img
+                        src={fijos.imagen || "/placeholder.svg"}
+                        alt={fijos.titulo}
+                      />
                     </div>
                     <div className="flex flex-col justify-between flex-grow">
-                      <h3 className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium w-[17ch] break-normal h-[32px]">
-                        {truncateText(fijo.titulo, 37)}
+                      <h3
+                        className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium  w-[17ch] break-normal h-[32px]"
+                      >
+                        {truncateText(fijos.titulo, 37)}
                       </h3>
                       <div className="flex justify-center mt-10 tablet:mt-4">
-                        <button className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light">
+                        <button
+                          className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light"
+                        >
                           Ir al sitio
                         </button>
                       </div>
@@ -333,20 +357,34 @@ function SeccionLigasInte() {
 
         {/* Remaining links carousel */}
         <div className="carrusel desktop-carousel">
-          <Slider {...settings} className="bg-white border tablet:border-0 border-slate-300 rounded-[1rem] mx-auto !z-5 w-full max-w-[300px] letras:max-w-[360px] ofertaEdu:max-w-[400px] tablet:max-w-[1150px] mt-8 px-4 tablet:px-0">
-            {restantes.map((restante, index) => (
+          <Slider
+            {...settings}
+            className="bg-white border tablet:border-0 border-slate-300 tablet:shadow-none rounded-lg tablet:rounded-none mx-auto !z-5 w-full max-w-[300px] letras:max-w-[360px] ofertaEdu:max-w-[400px] tablet:max-w-[1150px] mt-8 px-4 tablet:px-0"
+          >
+            {restantes.map((restantes, index) => (
               <div key={index} className="tablet:h-[450px] pt-4 tablet:pt-0">
-                <Link href={`/home-enlaces-de-interes/${restante.slug}`} target="_self" className="block h-full">
-                  <div className="border border-slate-300 shadow-none rounded-[1rem] h-full p-2 tablet:p-8 flex flex-col carousel-card">
+                <Link
+                  href={/home-enlaces-de-interes/${restantes.slug}}
+                  target="_self"
+                  className="block h-full"
+                >
+                  <div className="border-0 tablet:border border-slate-300 tablet:shadow-none rounded-none tablet:rounded-lg h-full p-2 tablet:p-8 flex flex-col carousel-card">
                     <div className="image-container mb-4">
-                      <img src={restante.imagen || "/placeholder.svg"} alt={restante.titulo} />
+                      <img
+                        src={restantes.imagen || "/placeholder.svg"}
+                        alt={restantes.titulo}
+                      />
                     </div>
                     <div className="flex flex-col justify-between flex-grow">
-                      <h3 className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium w-[17ch] break-normal h-[32px]">
-                        {truncateText(restante.titulo, 37)}
+                      <h3
+                        className="mt-4 px-2 tablet:px-5 text-center text-[16px] tablet:text-[22px] text-[#333334] font-medium w-[17ch] break-normal h-[32px]"
+                      >
+                        {truncateText(restantes.titulo, 37)}
                       </h3>
                       <div className="flex justify-center mt-10 tablet:mt-4">
-                        <button className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light">
+                        <button
+                          className="bg-[#611232] text-white text-xs letras:text-[13.5px] py-2 px-4 rounded-full hover:bg-white hover:text-[#611232] border-2 border-[#611232] font-light"
+                        >
                           Ir al sitio
                         </button>
                       </div>
