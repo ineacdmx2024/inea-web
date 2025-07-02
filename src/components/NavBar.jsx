bb14bac92f7a1de0b180584fcf046d5cf172723b
Jaret3000 <jaretitosayuso@gmail.com>
2025-07-02 15:17:51 -0600

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -114,8 +114,7 @@ const Navbar = () => {
                       Contacto
                     </Link>
                     <Link
-                      href="/"
-                      passHref
+                      href="/mapa-sitio"
                       className="hover:text-[#D3C09B] p-1 rounded"
                     >
                       Mapa del sitio
