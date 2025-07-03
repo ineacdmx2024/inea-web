3dc7b95aa9ca8b2acc3b843f2d72beb51ae5cfad
Jaret3000 <jaretitosayuso@gmail.com>
2025-07-02 19:40:17 -0600

    Update PlantillaPagMS.jsx

--- src/components/PlantillaPagMS.jsx
+++ src/components/PlantillaPagMS.jsx
@@ -12,22 +12,24 @@ const notoSans = Noto_Sans({
 function PagSec({ Titulo, children }) {
   return (
     <div className="mt-[10.5vh]">
-      <div className="mx-auto w-full medida3:w-4/5 md:w-[1280px] px-4 md:px-[2.5rem]">
+      <div className="mx-auto w-full medida3:w-4/5 md:w-[1280px] grid grid-cols-1 md:grid-cols-12 gap-8 items-start px-4 md:px-[2.5rem]">
         
         {/* Breadcrumb */}
-        <div className="py-1 px-0">
-          <Breadcrumb />
+        <div className="col-span-12">
+          <div className="mx-auto py-1 px-0">
+            <Breadcrumb />
+          </div>
         </div>
 
-        {/* Título */}
-        {Titulo && (
-          <h1 className={`${notoSans.className} text-[38px] font-semibold text-[#333334] mb-5 leading-tight`}>
-            {Titulo}
-          </h1>
-        )}
-
-        {/* Contenido */}
-        {children}
+        {/* Contenido principal */}
+        <div style={{ marginTop: '-20px' }} className="col-span-12 md:col-span-12">
+          {Titulo && (
+            <h1 className={`${notoSans.className} text-[38px] font-semibold text-[#333334] mb-5 leading-tight`}>
+              {Titulo}
+            </h1>
+          )}
+          {children}
+        </div>   
       </div>
     </div>
   );
