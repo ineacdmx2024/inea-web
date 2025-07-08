96e86b8e4c2fea9dc04bb13ec89f434cb18b0c5d
Jaret3000 <jaretitosayuso@gmail.com>
2025-07-07 19:49:37 -0600

    Update page.jsx

--- src/app/mapa-sitio/page.jsx
+++ src/app/mapa-sitio/page.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function MapaDelSitio() {
   return (
     <PagSec Titulo="Mapa del sitio">
-      <div className="space-y-6 text-[#333334] text-base sm:text-[15px]">
+      <div className="space-y-6 text-[#333334] text-base sm:text-[18px]">
         {/* Sección: Inicio */}
         <div>
           <h2 className="text-lg sm:text-xl font-semibold mb-2">Inicio</h2>
