d9c80dd2c82814f788fbd3aea328c2c53fcd1893
Jaret3000 <jaretitosayuso@gmail.com>
2025-08-04 20:45:17 -0600

    Update page.jsx

--- src/app/planeacion/page.jsx
+++ src/app/planeacion/page.jsx
@@ -168,12 +168,12 @@ function Planeacion() {
                   todosLosDatos().map((item, index) => (
                     <div
                       key={`${item.attributes.Fijo ? 'fijo' : 'normal'}-${index}`}
-                      className="overflow-hidden w-full max-w-[380px] mx-auto sm:mx-0 rounded-xl border border-slate-300 p-4 flex flex-col justify-between"
+                      className="overflow-hidden w-full max-w-[380px] mx-auto sm:mx-0 h-full min-h-[474px] rounded-xl border border-slate-300 p-4 flex flex-col justify-between"
                       style={{ height: "474px" }}
                     >
                       <div>
                         <Link href={`/planeacion/${item.attributes.slug}`}>
-                          <div className="rounded-xl overflow-hidden" style={{ width: "330.66px", height: "220px" }}>
+                          <div className="rounded-xl max-h-[220px] h-[220px] w-full overflow-hidden">
                             <Image
                               src={
                                 item.attributes.Imagen?.data?.attributes?.url ||
