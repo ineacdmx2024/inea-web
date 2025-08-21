9bdaec94e0dd651b00c6f3be52b538136a0a657b
Jaret3000 <jaretitosayuso@gmail.com>
2025-08-20 21:40:58 -0600

    Update CarouselOfertEdu.jsx

--- src/components/CarouselOfertEdu.jsx
+++ src/components/CarouselOfertEdu.jsx
@@ -135,9 +135,11 @@ function CarouselOfertEdu() {
     autoplaySpeed: 5000,
     dotsClass: "slick-dots custom-dots",
     appendDots: dots => (
-    <div className="flex justify-center w-full">
-      <ul className="m-0 p-0 flex relative">{dots}</ul>
-    </div>
+    // <div style={{ display: "flex", justifyContent: "center", width: "100%" }} --->cambio a tailwind >
+      <div className="flex justify-center w-full">
+        {/* <ul style={{ margin: 0, padding: 0, display: "flex" }}>{dots}</ul> --->cambio a tailwind */}
+        <ul className="m-0 p-0 flex ">{dots}</ul>
+      </div>
     ),
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
