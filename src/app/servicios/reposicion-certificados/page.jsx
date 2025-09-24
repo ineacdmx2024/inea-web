6d2f12f5104812da376244cd3733566d942e0632
Jaret3000 <jaretitosayuso@gmail.com>
2025-09-24 17:12:09 -0600

    Update page.jsx

--- src/app/servicios/reposicion-certificados/page.jsx
+++ src/app/servicios/reposicion-certificados/page.jsx
@@ -27,14 +27,14 @@ function Reposicion_certificados() {
       let enlaces = [];
       const fetchEnlacesL = async () => {
         const resPineados = await fetch(
-         // `https://inea-web-backend.onrender.com/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=true&populate=%2A`
-          `https://inea-web-backend-cg20.onrender.com/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=true&populate=%2A`
+         // `https://inea-web-backend-production.up.railway.app/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=true&populate=%2A`
+          `https://inea-web-backend-production.up.railway.app/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=true&populate=%2A`
         );
         const { data: enlacesPineados } = await resPineados.json();
         if (enlacesPineados.length < 3) {
           const resNoPineados = await fetch(
-            //`https://inea-web-backend.onrender.com/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=false&populate=%2A&sort[0]=Fecha:desc`
-             `https://inea-web-backend-cg20.onrender.com/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=false&populate=%2A&sort[0]=Fecha:desc`
+            //`https://inea-web-backend-production.up.railway.app/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=false&populate=%2A&sort[0]=Fecha:desc`
+             `https://inea-web-backend-production.up.railway.app/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=false&populate=%2A&sort[0]=Fecha:desc`
           );
           const { data: enlacesNoPineados } = await resNoPineados.json();
   
