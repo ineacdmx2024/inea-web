0ac71b992f5a073033c5f3b3925cc98e627ef58e
CesarINEA <elperreoteencanta@gmail.com>
2025-09-24 16:31:11 -0600

    mapa-sitio

--- src/app/mapa-sitio/page.jsx
+++ src/app/mapa-sitio/page.jsx
@@ -10,7 +10,7 @@ export default function MapaDelSitio() {
     useEffect(() => {
         const fetchEnlacesL = async () => {
         const resPineados = await fetch(
-            `https://inea-web-backend-cg20.onrender.com/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=true&populate=%2A`
+            `https://inea-web-backend-production.up.railway.app/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=true&populate=%2A`
         );
         const { data: enlacesPineados } = await resPineados.json();
 
@@ -18,7 +18,7 @@ export default function MapaDelSitio() {
 
         if (enlacesPineados.length < 3) {
             const resNoPineados = await fetch(
-            `https://inea-web-backend-cg20.onrender.com/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=false&populate=%2A&sort[0]=Fecha:desc`
+            `https://inea-web-backend-production.up.railway.app/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=false&populate=%2A&sort[0]=Fecha:desc`
             );
             const { data: enlacesNoPineados } = await resNoPineados.json();
 
