a5d0f9499218aa5d3d211b65fbe282286dab8a81
CesarINEA <elperreoteencanta@gmail.com>
2025-09-24 17:26:11 -0600

    servicio-social

--- src/app/servicio-social/page.jsx
+++ src/app/servicio-social/page.jsx
@@ -20,7 +20,7 @@ function ServicioSocial() {
   useEffect(() => {
     const fetchEnlacesL = async () => {
       const resPineados = await fetch(
-        `https://inea-web-backend-cg20.onrender.com/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=true&populate=%2A`
+        `https://inea-web-backend-production.up.railway.app/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=true&populate=%2A`
       );
       const { data: enlacesPineados } = await resPineados.json();
 
@@ -28,7 +28,7 @@ function ServicioSocial() {
 
       if (enlacesPineados.length < 3) {
         const resNoPineados = await fetch(
-          `https://inea-web-backend-cg20.onrender.com/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=false&populate=%2A&sort[0]=Fecha:desc`
+          `https://inea-web-backend-production.up.railway.app/api/enlaces-de-interes-laterales?filters[Pinear][$eq]=false&populate=%2A&sort[0]=Fecha:desc`
         );
         const { data: enlacesNoPineados } = await resNoPineados.json();
 
